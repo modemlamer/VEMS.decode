d567080f9a13ca4602652244fe9be0bda985b23a
modemlamer <felix@funits.de>
2021-10-03 13:10:49 +0200

    first

---
+++ readRound.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import time
+import serial
+
+ser = serial.Serial(
+        port='/dev/ttyUSB0',
+        baudrate = 19200,
+        parity=serial.PARITY_NONE,
+        stopbits=serial.STOPBITS_ONE,
+        bytesize=serial.EIGHTBITS,
+        timeout=1
+)
+
+
+
+while 1:
+
+
+        x=ser.readline().hex()
+        print (x)
+        print (".")
+        byteCounter=x.__len__()
+        print("bytecounter ist "+byteCounter.__str__())
+        if byteCounter == 18:
+                print("Startequence detected")
+        
+        if byteCounter == 80:
+                print("Datasequence  detected")
+
+        
+
+        #print (x.__len__())
+        time.sleep(1)
+        ser.write({0x41})
+        0
